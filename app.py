75cdf8fe953baa33cf0612e24161f6a10584b5b2
Nairo Elsner <nairo.elsner@hotmail.com>
2023-12-06 04:18:35 -0300

    update cors

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, request
-import json
+from flask_cors import CORS
 from social_network import *
 from test import *
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/users")
 def getUsers():
